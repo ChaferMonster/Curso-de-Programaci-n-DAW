1bf048fae263bdbe763172526d0f3d8cb7f0e9a0
Alvaro <alvarogarciachafer8@gmail.com>
2025-01-28 09:24:52 +0100

    Feat: Añadido de una prueba con Tkinter

--- Tema 3/NUEVO/Prueba.py
+++ Tema 3/NUEVO/Prueba.py
@@ -1,1 +1,35 @@
-Esto es una prueba subida a github
+import tkinter as tk 
+
+#Crear la ventana
+ventana = tk.Tk()
+
+#Configurar la ventana
+ventana.title("Mi primera app")
+ventana.geometry("900x700")
+
+#añadir la entrada de texto del usuario
+entrada = tk.Entry(ventana)
+entrada.pack()
+
+#Funcion para mostrar la entrada
+def mostrar_texto():
+    texto = entrada.get()
+    label.config(text=f"Escribiste: {texto}")
+
+#Crear una etiqueta
+label = tk.Label(ventana, text="Hola, mundo")
+label.pack() #Empaqueta el texto
+
+#Función para cambiar el texto
+def cambiar_texto():
+    label.config(text="Texto cambiado")
+
+#Crear un botón
+boton = tk.Button(ventana, text="Presióname",command=cambiar_texto)
+boton.pack()
+
+#Boton de entrada de texto
+boton2 = tk.Button(ventana, text="Mostrar texto escrito", command=mostrar_texto)
+boton2.pack()
+#Mostrar la ventana hasta que el usuario la cierra
+ventana.mainloop()

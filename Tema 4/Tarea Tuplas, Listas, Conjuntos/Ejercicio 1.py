6d88c5702073d41269f584d80ff3db6d32e27676
Alvaro <alvarogarciachafer8@gmail.com>
2025-02-14 09:40:16 +0100

    Nuevo tema 4

---
+++ Tema 4/Tarea Tuplas, Listas, Conjuntos/Ejercicio 1.py
@@ -0,0 +1,13 @@
+titulo = "La sombra"
+autor = "Stivenson"
+publicacion = 1300
+genero = "Terror"
+Precio = 350,55
+
+datos = titulo, autor, publicacion, genero
+
+libro1 = {datos:Precio}
+
+#He considerado guardar los datos del libro que no varían en el tiempo /
+# guardarlos en una tupla y finalmente guardar los datos en un diccionario donde /
+# podemos separar los datos del precio y poder cambiar este último 
